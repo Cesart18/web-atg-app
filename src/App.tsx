07f77bab21f73d4b8eaebedaa6b26359bcffff66
cesart18 <cjataonline@gmail.com>
2024-08-09 17:13:30 -0400

    correccion de error en router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Navbar } from './presentation/components/Navbar';
 import { AuthProvider } from './context/AuthContext';
-import { AppRouter } from './config/router/appRouter';
+import { AppRouter } from './config/router/AppRouter';
 import './index.css';
 
 
